7ef0a075c30e3395de83b4aa4d0a32507c82c302
admin <nastyabonybelay@gmail.com>
2023-04-25 09:22:47 +0300

    main

---
+++ SharelaneAutomation/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SharelaneAutomation;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
