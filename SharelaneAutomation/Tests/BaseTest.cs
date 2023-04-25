ad907ace5ee27ab229b033c9407b7952f40e495b
admin <nastyabonybelay@gmail.com>
2023-04-25 12:59:05 +0300

    add start test

---
+++ SharelaneAutomation/Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SharelaneAutomation.Login
+{
+    public class BaseTest
+    {
+        public WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
+        }
+    }
+}
+
---
+++ SharelaneAutomation/Tests/RegistrationLogin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace SharelaneAutomation.Login
+{
+    [TestFixture]
+
+    public class RegistrationLogin : BaseTest
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+            ChromeDriver.Navigate().GoToUrl("https://www.sharelane.com/cgi-bin/main.py");
+        }
+
+        [Test]
+        public void AR1_Login_ValidCredentianals()
+        {
+            //Var
+            string login = "brian_rao@840.03.sharelane.com";
+            string password = "1111";
+
+            //Action
+            ChromeDriver.FindElement(By.XPath("//*[@name='email']")).SendKeys(login);
+            ChromeDriver.FindElement(By.XPath("//*[@name='password']")).SendKeys(password);
+            ChromeDriver.FindElement(By.XPath("(//input)[5]")).Click();
+
+            //Assert
+            var logoutLink = ChromeDriver.FindElement(By.XPath("//a[text()='Logout']"));
+
+            Assert.IsNotNull(logoutLink.Displayed);
+        }
+
+        [Test]
+        public void AR3_Login_InvalidCredentianals()
+        {
+            //Var
+            string login = "brian_rao@840.03.sharelane.com";
+            string password = "11112";
+            string errorMessage = "Oops, error. Email and/or password don't match our records";
+
+            //Action
+            ChromeDriver.FindElement(By.XPath("//*[@name='email']")).SendKeys(login);
+            ChromeDriver.FindElement(By.XPath("//*[@name='password']")).SendKeys(password);
+            ChromeDriver.FindElement(By.XPath("(//input)[5]")).Click();
+
+            var error = ChromeDriver.FindElement(By.ClassName("error_message"));
+
+            //Assert
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(error.Text, errorMessage);
+                Assert.IsTrue(error.Displayed);
+            });
+        }
+
+
+    }
+}
+
